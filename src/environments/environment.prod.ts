07927e529b91bfc92d09f5b68fdc683568301131
almanza023 <46494649+almanza023@users.noreply.github.com>
2025-06-27 21:05:48 -0500

    Actualizar Endpoint Final.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,8 @@ export const environment = {
   production: true,
 
   //Back Local
-  baseURL:"http://localhost/apioasis/api/v1",
-  //baseURL: "https://api.mediexpress.com.co/api/v1"
-//baseURL: "https://apisincelejo.mediexpress.com.co/api/v1"
+  //baseURL:"http://localhost/apioasis/api/v1",
+  baseURL: "https://api.comercializadoraoasis.com/api/v1"
 
 
 };
